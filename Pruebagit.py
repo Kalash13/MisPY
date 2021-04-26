9a57994423457553dc5875810610421d8acc9a2b
Kalash13 <failkernel.12@gmail.com>
2021-04-26 03:55:43 -1200

    modified add peso to dolar converter

--- Pruebagit.py
+++ Pruebagit.py
@@ -1,1 +1,12 @@
-print ("Esto es una prueba de git")
+
+
+"""Conversor de monedas de peso chileno a dolar"""
+
+pesos = int(input("Cantidad de pesos chilenos"))
+
+dolar = 0.0014
+
+
+conversion = dolar*pesos
+
+print (f"El valor del peso chileno a dolar es: $ {conversion:.4} dolares")
