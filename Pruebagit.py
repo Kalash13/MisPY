f6b0750e95017aa33be3d0b932b5b0085b23c688
Kalash13 <failkernel.12@gmail.com>
2021-05-08 11:50:06 -1200

    se agrega apunte de true y false para entender como funciona

--- Pruebagit.py
+++ Pruebagit.py
@@ -2,22 +2,33 @@
 
 """ Conversor de monedas de peso chileno a dolar"""
 
-# print ("Ingrese la cantiadad de pesos chilenos para converitr a dolar: ")
-# pesos = int (input(" "))
+print ("Ingrese la cantiadad de pesos chilenos para converitr a dolar: ")
+pesos = int (input(" "))
 
-# dolar = 0.001418
+dolar = 0.001418
 
 
-# conversion = dolar*pesos
+conversion = dolar*pesos
 
-# print (f"Peso chileno a dolar es: $ {conversion:.3} dolares")
+print (f"Peso chileno a dolar es: $ {conversion:.3} dolares")
 
-# print("Exchange rate dollars to chilean pesos")
-# dollar = float (input("Enter the amount of dollars to convert to Chilean peso:  "))
-# pesos = 705.05
-# conversion = dollar * pesos
+print("Exchange rate dollars to chilean pesos")
+dollar = float (input("Enter the amount of dollars to convert to Chilean peso:  "))
+pesos = 705.05
+conversion = dollar * pesos
 
-# print(f"Dollars to chilean pesos: {conversion:.6}")
+print(f"Dollars to chilean pesos: {conversion:.6}")
 
-pivot = es_estudiante and trabaja
-print(f'Es estudiante Y trabaja: {pivot}')
+# a = True
+# b = True
+# c = False
+# d = True
+# e = False
+
+# print(a or b)
+# print(a or c)
+# print(a and e)
+# print(a or e) and b
+# print (a or e) and c
+# print(a or e) and (c or b)
+# print((a or b) and c) or e
